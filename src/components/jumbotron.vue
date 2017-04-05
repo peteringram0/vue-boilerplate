442a2ee9e1f6d179095081e66b9300773503ff86
Peter Ingram <peter.ingram0@gmail.com>
2017-04-05 22:23:12 +0100

    Playing with event binding

--- src/components/jumbotron.vue
+++ src/components/jumbotron.vue
@@ -3,21 +3,35 @@
 		<div class="container">
 	    	<h1 class="display-3">Fluid jumbotron</h1>
 	    	<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+			<button class="btn btn-primary" @click="notifyparent">Notify parent</button>
+			<button class="btn btn-primary" @click="runFromChild">Run parent function from child</button>
 	  	</div>
 	</div>
 </template>
 
 <script>
 export default {
-	// props: [],
+	props: ['function-for-child'],
 
 	data() {
 		return {
-			// containerClass: 'uk-container uk-light'
+
 		}
 	},
 	computed: {
 
+	},
+	methods: {
+
+		// Methods should be functions like this <- ES6 methods wont pass through the correct .this context
+		notifyparent() {
+			this.$emit('notifyparent');
+		},
+
+		// Run parent method from the child
+		runFromChild() {
+			this.functionForChild();
+		}
 	},
 	mounted() {
 		// console.log(this.containerClass);
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,35 @@
 <template>
-	<section>
-		<jumbotron></jumbotron>
-		<jumbotron></jumbotron>
-	</section>
+<section>
+	<jumbotron></jumbotron>
+	<jumbotron @notifyparent="notify" :function-for-child="runInsideChild"></jumbotron>
+	<input type="text" v-model="text"> text: {{ text }}
+</section>
 </template>
 
 <script>
-
 import Jumbotron from './../components/jumbotron.vue';
 
 export default {
 	components: {
 		Jumbotron
+	},
+	watch: {
+		text: function(val) {
+			console.log('Testing watchers. New val: ' + val);
+		}
+	},
+	methods: {
+
+		// Notify from child to trigger this
+		notify() {
+			alert('PING FROM PARENT');
+		},
+
+		// Check a child can run this
+		runInsideChild() {
+			alert('This function is in home.vue but ran in a child component');
+		},
+
 	},
 	data() {
 		return {
