8a2a426d48d06a9ae582452e561a0be0055fd9d4
Peter Ingram <p.ingram@managedracks.co.uk>
2017-07-14 21:22:41 +0100

    working on testing

--- test/nav.spec.js
+++ test/nav.spec.js
@@ -1,12 +1,47 @@
+import Vue from 'vue';
 import test from 'ava';
-import { mount } from 'avoriaz';
+// import { mount } from 'avoriaz';
+
+var vm;
 
 import NavBar from './../src/components/nav.vue';
 
+//
+// test('Check expanding & collapsing the navbar', t => {
+//
+//     let wrapper = mount(NavBar, {
+//         propsData: {
+//             items: [{
+//                 name: 'Home',
+//                 url: '/home'
+//             }, {
+//                 name: 'About',
+//                 url: '/about'
+//             }]
+//         }
+//     });
+//
+//     // t.true(wrapper.hasClass('nav'));
+//
+//     wrapper.toggleNav();
+//
+// 	// t.is(vm.navExpanded, false);
+// 	// vm.toggleNav();
+// 	// t.is(vm.navExpanded, 'is-active');
+//     //
+// 	// t.is(vm.navExpanded, 'is-active');
+// 	// vm.toggleNav();
+// 	// t.is(vm.navExpanded, false);
+//
+// });
 
-test('Check expanding & collapsing the navbar', t => {
 
-    let wrapper = mount(NavBar, {
+test('Check if dropdown list is toggling on click', t => {
+
+    let N = Vue.extend(NavBar);
+
+    // Make our fake component
+    vm = new N({
         propsData: {
             items: [{
                 name: 'Home',
@@ -15,19 +50,25 @@ test('Check expanding & collapsing the navbar', t => {
                 name: 'About',
                 url: '/about'
             }]
-        }
-    });
+        },
+    }).$mount();
 
-    // t.true(wrapper.hasClass('nav'));
+    // Bind a router
+    // vm.$router = new VueRouter();
 
-    // console.log(wrapper.name);
+    // By default isActive should be false
+    t.is(vm.navExpanded, false);
 
-	// t.is(vm.navExpanded, false);
-	// vm.toggleNav();
-	// t.is(vm.navExpanded, 'is-active');
-    //
-	// t.is(vm.navExpanded, 'is-active');
-	// vm.toggleNav();
-	// t.is(vm.navExpanded, false);
+    // Trigger function
+    vm.toggleNav();
+
+    // isActive should now be true
+    t.is(vm.navExpanded, 'is-active');
+
+    // Trigger function
+    vm.toggleNav();
+
+    // By default isActive should be false
+    t.is(vm.navExpanded, false);
 
 });
