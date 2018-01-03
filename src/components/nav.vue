3710d800b699c17e9ec2ba98bfc14e585b378234
Peter Ingram <p.ingram@managedracks.co.uk>
2018-01-03 12:43:15 +0000

    example of a spy, tests now run under PHPStorm

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,51 +1,59 @@
 <template lang="pug">
-header.nav
-	.container
-		.nav-left
-			a.nav-item.is-brand
-				img(alt='logo')
-		span.nav-toggle(@click="toggleNav", :class="navExpanded")
-			span
-			span
-			span
-		.nav-right.nav-menu(:class="navExpanded")
-			router-link.nav-item.is-tab(v-for='item in items', :key="item.url", :to='item.url', v-if='!item.children') {{ item.name }}
-			a.nav-item.is-tab Log out
+    header.nav
+        .container
+            .nav-left
+                a.nav-item.is-brand
+                    img(alt='logo')
+            span.nav-toggle(@click="toggleNav", :class="navExpanded")
+                span
+                span
+                span
+            .nav-right.nav-menu(:class="navExpanded")
+                router-link.nav-item.is-tab(v-for='item in items', :key="item.url", :to='item.url', v-if='!item.children') {{ item.name }}
+                a.nav-item.is-tab Log out
 </template>
 
 <style lang="stylus" scoped>
-.nav-item
-	&.is-active, &:hover
-		border-bottom-color white !important
+    .nav-item
+        &.is-active, &:hover
+            border-bottom-color white !important
 </style>
 
 <script>
-export default {
-	props: ['items'],
-	data() {
-		return {
-			navExpanded: false
-		}
-	},
-	methods: {
+    export default {
+        props: ['items'],
+        data() {
+            return {
+                navExpanded: false
+            }
+        },
+        methods: {
 
-		/**
-		 * Toggle the active state on the navigation
-		 */
-		toggleNav() {
-			this.navExpanded = ((!this.navExpanded) ? 'is-active' : false)
-		}
+            /**
+             * Toggle the active state on the navigation
+             */
+            toggleNav() {
+                this.navExpanded = ((!this.navExpanded) ? 'is-active' : false);
+            },
 
-	},
-	watch: {
+            testSpy() {
+                this.otherMethod();
+            },
 
-		/**
-		 * On route changes collapse the nav
-		 */
-		$route() {
-			this.navExpanded = false;
-		}
+            otherMethod() {
+                console.log('Triggered');
+            }
 
-	}
-}
+        },
+        watch: {
+
+            /**
+             * On route changes collapse the nav
+             */
+            $route() {
+                this.navExpanded = false;
+            }
+
+        }
+    }
 </script>
--- test/component.nav.spec.js
+++ test/component.nav.spec.js
@@ -1,5 +1,5 @@
 // test/component-a.spec.js
-let Vue = require('vue');
+let Vue = require('vue/dist/vue.min');
 let Nav = require('./../src/components/nav.vue').default;
 
 describe('nav.vue', function () {
@@ -18,6 +18,19 @@ describe('nav.vue', function () {
         vm.toggleNav();
 
         expect(vm.navExpanded).toBe('is-active');
+
+    });
+
+    it('Testing spys', function () {
+
+        let Constructor = Vue.extend(Nav);
+        let vm = new Constructor().$mount();
+
+        spyOn(vm, 'otherMethod');
+
+        vm.testSpy();
+
+        expect(vm.otherMethod).toHaveBeenCalled();
 
     });
 
